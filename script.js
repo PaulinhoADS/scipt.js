4d665f46dc7e9bfe7428c1b408e585946a175a9d
Paulo Cesar Leite <paulinho.vivi@hotmail.com>
2025-06-26 20:12:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js - Estrutura em branco
+
+alert ("Comando para mostar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo o codigo");
+
+alert ("Ele é limitado ao clique no ok pelo usuário");
+
+alert ("Não é possível configurar seu tempo de permanencia na tela");
