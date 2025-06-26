3dc22dde75bf85fee26460693eed01ae3445595b
Paulo Cesar Leite <paulinho.vivi@hotmail.com>
2025-06-26 20:17:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// script.js - Estrutura em branco
+// script.js - primeiro codigo
 
 alert ("Comando para mostar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo o codigo");
 
 alert ("Ele é limitado ao clique no ok pelo usuário");
 
 alert ("Não é possível configurar seu tempo de permanencia na tela");
+
+let numero = 25;
+let chute;
+let idade;
+let nome;
+
+console.log (numero);
